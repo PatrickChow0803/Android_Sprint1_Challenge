bea75b9247f0cfba5b5b08221871795ca9ca91ed
Patrick Chow <PatrickChow0803@gmail.com>
2019-08-09 11:35:18 -0400

    Created Explicit Intents for both List and Edit buttons

--- app/src/main/java/com/patrickchow/sprintchallenge1/ui/EditActivity.kt
+++ app/src/main/java/com/patrickchow/sprintchallenge1/ui/EditActivity.kt
@@ -1,13 +1,28 @@
 package com.patrickchow.sprintchallenge1.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.patrickchow.sprintchallenge1.R
+import com.patrickchow.sprintchallenge1.model.Movie
+import kotlinx.android.synthetic.main.activity_edit.*
 
 class EditActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+
+        title = "Edit Movie"
+
+        btn_save_movie.setOnClickListener{
+            var intentSaveMovie = Intent()
+
+        }
+
+        fun createMovie(): Movie{
+            var newMovie = Movie(edit_text_add_movie.text.toString(), false)
+            return newMovie
+        }
     }
 }
--- app/src/main/java/com/patrickchow/sprintchallenge1/ui/ListActivity.kt
+++ app/src/main/java/com/patrickchow/sprintchallenge1/ui/ListActivity.kt
@@ -1,16 +1,30 @@
 package com.patrickchow.sprintchallenge1.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.patrickchow.sprintchallenge1.R
+import com.patrickchow.sprintchallenge1.model.Movie
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ListActivity : AppCompatActivity() {
+
+    var movieList = mutableListOf<Movie>()
+    var counter = 0
+
+    companion object{
+        const val REQUEST_CODE_EDIT_MOVIE = 10
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        title = "List of Movies"
 
-
+        btn_add_movie.setOnClickListener{
+            var intent = Intent(this, EditActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE_EDIT_MOVIE)
+        }
     }
 }
